ea374bad68ab9adba9a417607a9773d0c396a08b
Javier Martin <jmf.5453@gmail.com>
2018-05-25 20:05:56 +0200

    Chat No-rooms Funciona

--- assets/js/socket.io.js
+++ assets/js/socket.io.js
@@ -1,21 +1,45 @@
-$(function() {
+$(document).ready(function() {
     var socket = io.connect();
 
-    socket.on('connect', function(){
-        socket.emit('addUser', getCookie("name"));
+    var name = getCookie("name");
+
+    socket.on('connect', function() {
+        socket.emit('addUser', name);
     });
 
-    socket.on('updateChat', function(username, data){
-        $(".chat-history").append('<div class="msg-user"><div>' + username +
-        "</div><div>" + data + "</div></div>");
+    socket.on('updateChat', function(username, data) {
+        if (username == name) {
+            $(".chat-history").append('<div class="my-msg"><div>' + username +
+                "</div><div>" + data + "</div></div>");
+        } else {
+            $(".chat-history").append('<div class="msg"><div>' + username +
+                "</div><div>" + data + "</div></div>");
+        }
+
+        $('.chat-history').animate({ scrollTop: $(document).height() }, 1000);
     });
 
-    $(function(){
-        $("#sendMsg").click(function(){
+    $(function() {
+        $("#sendMsg").click(function() {
             var message = $('#msgContent').val();
-            $('#data').val('');
+            $('#msgContent').val('');
             socket.emit('sendChat', message);
         });
-    })
+    });
 
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
 });
